3d3bf4d8ef9a5c949a0a33315dc59d21f10c458d
jerismith32 <jerismith32@gmail.com>
2021-08-09 19:48:44 -0400

    updated password with dotenv

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'Dobby.love32!',
+      password: 'process.env.PASSWORD',
       database: 'election'
     },
     console.log('Connected to the election database.')

677299aa7f3f1e35d9385f90149cfe1c712b24ea
jerismith32 <jerismith32@gmail.com>
2021-08-09 19:38:25 -0400

    updated u-develop it

---
+++ server.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'jerismith32@gmail.com',
+      // Your MySQL password
+      password: 'Dobby.love32!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Route to handle user requests that aren't supported by the app
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
